3dcd6e0181ca47ae198d0551e68aaab93c7394e0
Francisco Vallone <vallonefrancisco@gmail.com>
2023-12-06 17:15:09 -0300

    se agregaron nuevas logicas para el juego y la constante comboWinner para saber el premio del ganador

--- src/logics/logics.js
+++ src/logics/logics.js
@@ -1,14 +1,17 @@
-import {DataTypeBoard} from '../logics/constants'
+import {DataTypeBoard , ComboWinner} from '../logics/constants'
 
 // GENERA EL TABLERO CON LAS MINAS Y LOS DIAMANTES
 export function MineAleatory(sizeArray,numMines){
-    let Mines = Array(sizeArray).fill(null)
+    let Mines = Array(25).fill().map(()=>({
+        type: null,
+        check: false
+    }))
     for(let i=0 ; i < numMines ; i++){
         ArrayWhitMines(sizeArray,Mines)
     }
     let newArray = Mines.map(x => {
-        if(x == null){
-            x = DataTypeBoard.diamond
+        if(x.type == null){
+            x.type = DataTypeBoard.diamond
         }
         return x
     })
@@ -16,13 +19,13 @@ export function MineAleatory(sizeArray,numMines){
 }
 
 // GENERA UN ARREGLO CON LAS MINAS
-function ArrayWhitMines(sizeArray,data){
+function ArrayWhitMines(sizeArray,arraydata){
     let number = getRandomInt(sizeArray);
-    if(data[number] == null){
-        data[number] = DataTypeBoard.mine;
+    if(arraydata[number].type == null){
+        arraydata[number].type = DataTypeBoard.mine;
         return
     }
-    ArrayWhitMines(sizeArray,data)
+    ArrayWhitMines(sizeArray,arraydata)
 }
 
 // GENERA UN NUMERO ALEATORIO
@@ -30,5 +33,20 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-MineAleatory(25,4);
-// console.log(DataTypeBoard)
+// BUSCAR DIAMANTES RESTANTES
+export function searchRemainingDiamond(board){
+    let remainingDiamond = board.filter( x => (x.type == DataTypeBoard.diamond && x.check == false))
+    return remainingDiamond.length == 0 ?  true : false;
+}
+
+// BUSCAR MINAS
+export function searchMines(board,index){
+    return board[index].type == DataTypeBoard.mine ? true : false;
+}
+
+// CALCULAR PREMIO 
+export function calculatePrize(mines,foundDiamond){
+    return foundDiamond > 0 ? ComboWinner[mines][foundDiamond-1] : 0;
+}
+
+// console.log(MineAleatory(25,24));
