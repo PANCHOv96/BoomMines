51444184d1d837813197bf7e91c7b6223fcfadc3
Francisco Vallone <vallonefrancisco@gmail.com>
2023-12-08 08:00:36 -0300

    se añade el footer a la aplicacion con sus estilos de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/navbar'
 import Game from './components/game/game'
+import Footer from './components/Footer/footer'
 
 function App() {
   return (
     <>
       <Navbar/>
       <Game/>
+      <Footer/>
     </>
   )
 }
